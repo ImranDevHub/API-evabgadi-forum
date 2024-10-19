88f3c695eee4c478a04bb5fafc136c8a02b2dce0
Imran Muhammad <168343187+ImranDevHub@users.noreply.github.com>
2024-10-19 15:05:03 +0300

    Update db.config.js

--- database/db.config.js
+++ database/db.config.js
@@ -4,10 +4,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const connection = mysql2.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
     connectionLimit: 10
 });
 
