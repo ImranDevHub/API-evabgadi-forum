85dbb5a37e0105ed84be253ae502743bc7976670
Imran Muhammad <168343187+ImranDevHub@users.noreply.github.com>
2024-10-19 14:27:36 +0300

    Update db.config.js

--- database/db.config.js
+++ database/db.config.js
@@ -7,7 +7,7 @@ const connection = mysql2.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: process.env.DB_DATABASE,
     connectionLimit: 10
 });
 
